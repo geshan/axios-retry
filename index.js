1fb19ba4e0c4faefc1c2854b099bf52f2aee6903
Geshan Manandhar <geshan@simplywallst.com>
2023-09-25 21:19:12 +1000

    Adds initial working example

---
+++ index.js
@@ -0,0 +1,36 @@
+import axiosRetry from 'axios-retry';
+import axios from 'axios';
+ 
+axiosRetry(axios, { 
+   retries: 3,
+   retryDelay: axiosRetry.exponentialDelay,
+   retryCondition(error) {
+    switch (error.response.status) {
+      //retry only if status is 500 or 501
+      case 500:
+      case 501:
+        return true;
+      default:
+        return false;
+      }
+    },
+    onRetry: (retryCount, error, requestConfig) => {
+      console.log(`retry count: `, retryCount);
+      if(retryCount == 2) {
+        requestConfig.url = 'https://postman-echo.com/status/200';
+      }
+    },
+});
+ 
+(async () => {
+  try {
+    const res = await axios.get('https://postman-echo.com/status/500');
+    console.log(`inside async:`, res.status);
+  } catch(err) {
+    console.error(`Error occured: `, err.message);
+  }  
+})();
+
+//top level await is possible as this is a module and running with Node 18+
+//const res2 = await axios.get('https://postman-echo.com/status/500');
+//console.log(`top level await:`, res2.status);
